98d372d3717c8c8b4ed68be2e6b31560793cf8a2
Filipe Araujo <filipe.alves.araujo@gmail.com>
2017-09-10 20:07:55 -0400

    set element to use shadowroot instead of innerHTML. use property accessors to enable polymer syntax observers

--- src/poly-lit-element.js
+++ src/poly-lit-element.js
@@ -1,11 +1,19 @@
-import {Element} from '@polymer/polymer/polymer-element';
-import {PropertyAccessors} from '@polymer/polymer/lib/mixins/property-accessors';
-import {render} from 'lit-html';
+import { Element } from '@polymer/polymer/polymer-element';
+import { PropertyAccessors } from '@polymer/polymer/lib/mixins/property-accessors';
+import { PropertyEffects } from '@polymer/polymer/lib/mixins/property-effects';
+import { render } from 'lit-html';
 
 const withLit = Base => {
-  return class extends PropertyAccessors(Base) {
-    _propertiesChanged() {
-      render(this.render(), this);
+  return class extends PropertyEffects(PropertyAccessors(Base)) {
+    // render as well as trigger original _propertiesChanged method
+    _propertiesChanged(...args) {
+      super._propertiesChanged(...args);
+      render(this.render(), this.shadowRoot);
+    }
+
+    // prevents __templateInstance error during _propertiesChanged() execution
+    static get template() {
+      return '<!--empty-->';
     }
   };
 };
